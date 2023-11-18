a6ad06b9da01d66cdd5696c6501a99a669e7859d
janZ <janzander96@gmail.com>
2023-11-18 11:06:33 +0800

    added timestamp

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,3 +1,7 @@
 const lastModifiedElement = document.querySelector("#lastModified");
+const timeStampElement = document.querySelector("#timestamp");
+const currentDate = new Date();
 
 lastModifiedElement.textContent = new Date(document.lastModified);
+
+timeStampElement.textContent = currentDate;
