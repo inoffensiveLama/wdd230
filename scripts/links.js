ae306e2809216dc3bfa2b6cc408afb00b47bfcc6
janZ <janzander96@gmail.com>
2023-11-30 06:57:38 +0800

    created json

---
+++ scripts/links.js
@@ -0,0 +1,38 @@
+const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+const displayLinks = (data) => {
+    let i = 0;
+    data.forEach(week => {
+        const linkParent = document.querySelector("#linkMenu");
+        let listElement = document.createElement("li");
+        listElement.innerHTML += `Week0${i}: `;
+
+        week.foreach(link => {
+            let linkElement = document.createElement("a");
+
+            linkElement.setAttribute("href", link.url);
+            listElement.innerHTML += ` | `;
+            listElement.appendChild(linkElement);
+        });
+
+        linkParent.appendChild(listElement);
+        
+        i++;
+    });
+}
+
+  
+getLinks();
---
+++ scripts/weather.js
@@ -0,0 +1,38 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDescr = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=14.60&lon=120.99&appid=d2234c90500fb15ffdcf322d4bc8b5b1&units=imperial"
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            let data = await response.json();
+            displayResult(data);
+        }else{
+            throw Error(await response.text());
+        }
+
+    } catch(error){
+        console.log(error);
+    }
+    
+
+}
+
+function displayResult(data){
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`
+    let iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let iconDescr = `${data.weather[0].description}`
+
+    weatherIcon.setAttribute("src", iconSrc);
+    weatherIcon.setAttribute("alt", iconDescr);
+
+    captionDescr.textContent = `${iconDescr}`;
+}
+
+apiFetch();
+
+
+
